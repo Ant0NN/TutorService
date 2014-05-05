bfbed4bf14922deae09bcac65522c4f0fefdf736
MikheevDmitry <mikheevdmitry94@gmail.com>
2014-05-05 13:02:37 +0400

    fix

--- BaseApp/decorators.py
+++ BaseApp/decorators.py
@@ -11,7 +11,7 @@ def no_login_please(f):
     def wrapper(request, *args, **kargs):
         user = request.user
         if user.is_authenticated():
-            return HttpResponseRedirect(reverse("tutor_service:home"))
+            return HttpResponseRedirect(reverse("tutor_service:homepage"))
         else:
             return f(request, *args, **kargs)
 
@@ -23,7 +23,7 @@ def check_add_subject__for_tutor(f):
         try:
             Tutor.objects.get(username=self.request.user.id)
         except ObjectDoesNotExist:
-            return HttpResponseRedirect(reverse("tutor_service:home"))
+            return HttpResponseRedirect(reverse("tutor_service:homepage"))
         return f(self, request, *args, **kwargs)
 
     return wrapper
@@ -34,7 +34,7 @@ def check_questionnaire__for_tutor(f):
         try:
             Tutor.objects.get(username=request.user.id)
         except ObjectDoesNotExist:
-            return HttpResponseRedirect(reverse("tutor_service:home"))
+            return HttpResponseRedirect(reverse("tutor_service:homepage"))
         return f(request)
 
     return wrapper
@@ -44,5 +44,5 @@ def is_anonymous(f):
         if request.user.is_anonymous():
             return f(request, *args, **kwargs)
         else:
-            return HttpResponseRedirect(reverse("tutor_service:home"))
+            return HttpResponseRedirect(reverse("tutor_service:homepage"))
     return wrapper
